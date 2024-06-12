7b15eeb9200b5bbd027b87ccb3d749a792960e6d
sankargreet <sankargreet@gmail.com>
2024-06-12 10:13:03 -0500

    Update test.cs

--- test.cs
+++ test.cs
@@ -1,103 +1,118 @@
-import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
-import software.amazon.awssdk.regions.Region;
-import software.amazon.awssdk.services.s3.S3Client;
-import software.amazon.awssdk.services.s3.model.GetObjectRequest;
-import software.amazon.awssdk.services.s3.model.GetObjectResponse;
-import software.amazon.awssdk.services.s3.model.HeadObjectRequest;
-import software.amazon.awssdk.services.s3.model.HeadObjectResponse;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using Amazon;
+using Amazon.S3;
+using Amazon.S3.Model;
+using Amazon.S3.Util;
 
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        string bucketName = "your-bucket-name";
+        string objectKeyPrefix = "your-object-key-prefix";
+        string region = "us-west-2"; // Specify your region
+        int urlValidityDurationInMinutes = 60; // Duration for which the URL is valid
 
-public class S3FileChunkDownloader {
+        var s3Client = new AmazonS3Client(Amazon.RegionEndpoint.GetBySystemName(region));
 
-    private static final String BUCKET_NAME = "your-bucket-name";
-    private static final String OBJECT_KEY = "your-large-file.txt";
-    private static final Region REGION = Region.US_EAST_1; // Replace with your region
-    private static final long CHUNK_SIZE = 512 * 1024 * 1024; // 0.5 GB in bytes
+        // Simulate receiving file content from a producer
+        byte[] fileContent = Encoding.UTF8.GetBytes("This is the content of the file.");
+        
+        var postResponse = GeneratePreSignedPost(s3Client, bucketName, objectKeyPrefix, urlValidityDurationInMinutes, fileContent);
 
-    public static void main(String[] args) throws IOException {
-        S3Client s3Client = S3Client.builder()
-                .region(REGION)
-                .credentialsProvider(ProfileCredentialsProvider.create())
-                .build();
-
-        downloadFileInChunks(s3Client);
+        Console.WriteLine("Pre-Signed POST URL: " + postResponse.Url);
+        foreach (var field in postResponse.Fields)
+        {
+            Console.WriteLine($"{field.Key}: {field.Value}");
+        }
     }
 
-    private static void downloadFileInChunks(S3Client s3Client) throws IOException {
-        long fileSize = getFileSize(s3Client);
-        long totalBytesRead = 0;
-        int chunkNumber = 0;
-
-        GetObjectRequest getObjectRequest = GetObjectRequest.builder()
-                .bucket(BUCKET_NAME)
-                .key(OBJECT_KEY)
-                .build();
-
-        try (InputStream responseStream = s3Client.getObject(getObjectRequest)) {
-            while (totalBytesRead < fileSize) {
-                String chunkFilePath = "chunk_" + chunkNumber + ".txt";
-                long bytesToRead = Math.min(CHUNK_SIZE, fileSize - totalBytesRead);
-
-                long bytesReadInChunk = writeChunkToFile(responseStream, chunkFilePath, bytesToRead);
-
-                totalBytesRead += bytesReadInChunk;
-                chunkNumber++;
-
-                if (bytesReadInChunk == 0) {
-                    break;
-                }
+    static PostResponse GeneratePreSignedPost(IAmazonS3 s3Client, string bucketName, string objectKeyPrefix, int validityDurationInMinutes, byte[] fileContent)
+    {
+        // Create the policy conditions
+        var conditions = new List<PostPolicyCondition>
+        {
+            new PostPolicyCondition
+            {
+                Type = PostPolicyConditionType.StartsWith,
+                Condition = new[] { "key", objectKeyPrefix }
+            },
+            new PostPolicyCondition
+            {
+                Type = PostPolicyConditionType.ContentType,
+                Condition = new[] { "text/plain" }
+            },
+            new PostPolicyCondition
+            {
+                Type = PostPolicyConditionType.ContentType,
+                Condition = new[] { "application/zip" }
+            },
+            new PostPolicyCondition
+            {
+                Type = PostPolicyConditionType.ContentType,
+                Condition = new[] { "text/csv" }
+            },
+            new PostPolicyCondition
+            {
+                Type = PostPolicyConditionType.Acl,
+                Condition = new[] { "private" }
+            },
+            new PostPolicyCondition
+            {
+                Type = PostPolicyConditionType.ContentLengthRange,
+                Condition = new[] { "0", "10485760" } // Max file size 10 MB
+            },
+            new PostPolicyCondition
+            {
+                Type = PostPolicyConditionType.XAmzAlgorithm,
+                Condition = new[] { "AWS4-HMAC-SHA256" }
             }
-        }
+        };
 
-        System.out.println("Chunks downloaded successfully.");
+        var expiration = DateTime.UtcNow.AddMinutes(validityDurationInMinutes);
+
+        // Create the post policy
+        var policy = new S3PostPolicy
+        {
+            Expiration = expiration,
+            Conditions = conditions
+        };
+
+        // Generate the pre-signed POST URL
+        var postUrl = AmazonS3Util.GeneratePostPreSignedUrl(s3Client, bucketName, objectKeyPrefix, expiration, policy);
+
+        // Compute Content-MD5
+        var md5Hash = ComputeMD5(fileContent);
+
+        var response = new PostResponse
+        {
+            Url = postUrl.Uri.ToString(),
+            Fields = postUrl.Fields
+        };
+
+        // Add Content-MD5 to the fields
+        response.Fields.Add("Content-MD5", md5Hash);
+
+        return response;
     }
 
-    private static long getFileSize(S3Client s3Client) {
-        HeadObjectRequest headObjectRequest = HeadObjectRequest.builder()
-                .bucket(BUCKET_NAME)
-                .key(OBJECT_KEY)
-                .build();
-
-        HeadObjectResponse headObjectResponse = s3Client.headObject(headObjectRequest);
-        return headObjectResponse.contentLength();
-    }
-
-    private static long writeChunkToFile(InputStream responseStream, String chunkFilePath, long bytesToRead) throws IOException {
-        long bytesReadInChunk = 0;
-        long lastNewlinePosition = 0;
-        byte[] buffer = new byte[8192];
-
-        try (FileOutputStream fileOutputStream = new FileOutputStream(chunkFilePath)) {
-            int bytesRead;
-            while (bytesReadInChunk < bytesToRead && (bytesRead = responseStream.read(buffer)) > 0) {
-                bytesReadInChunk += bytesRead;
-
-                for (int i = 0; i < bytesRead; i++) {
-                    if (buffer[i] == '\n') {
-                        lastNewlinePosition = bytesReadInChunk - (bytesRead - i - 1);
-                    }
-                }
-
-                fileOutputStream.write(buffer, 0, bytesRead);
-            }
-        }
-
-        if (lastNewlinePosition < bytesReadInChunk) {
-            adjustFileToLastNewline(chunkFilePath, lastNewlinePosition);
-            bytesReadInChunk = lastNewlinePosition;
-        }
-
-        return bytesReadInChunk;
-    }
-
-    private static void adjustFileToLastNewline(String filePath, long position) throws IOException {
-        try (FileOutputStream fileOutputStream = new FileOutputStream(filePath, true)) {
-            fileOutputStream.getChannel().truncate(position);
+    static string ComputeMD5(byte[] content)
+    {
+        using (var md5 = MD5.Create())
+        {
+            var hash = md5.ComputeHash(content);
+            return Convert.ToBase64String(hash);
         }
     }
+}
+
+public class PostResponse
+{
+    public string Url { get; set; }
+    public IDictionary<string, string> Fields { get; set; }
 }
